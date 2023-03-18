f0998aa4686f4c7debfc1f52b3709baa6ed0c789
Gamliel Urlanda <g.urlanda@gmail.com>
2023-03-18 00:24:25 -0700

    Add logo imports

---
+++ src/assets/img/logos/logos.ts
@@ -0,0 +1,35 @@
+import bulma from './bulma-logo.svg';
+import css3 from './css3-logo.svg';
+import expressjs from './expressjs-logo.svg';
+import git from './git-logo.svg';
+import github from './github-logo.svg';
+import html5 from './html5-logo.svg';
+import js from './js-logo.svg';
+import mongodb from './mongodb-logo.svg';
+import nodejs from './nodejs-logo.svg';
+import npmjs from './npmjs-logo.svg';
+import python from './python-logo.svg';
+import reactjs from './reactjs-logo.svg';
+import sass from './sass-logo.svg';
+import tailwindcss from './tailwindcss-logo.svg';
+import typescript from './typescript-logo.svg';
+
+const logos = [
+  bulma,
+  css3,
+  expressjs,
+  git,
+  github,
+  html5,
+  js,
+  mongodb,
+  nodejs,
+  npmjs,
+  python,
+  reactjs,
+  sass,
+  tailwindcss,
+  typescript,
+];
+
+export default logos;
