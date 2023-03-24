c9a21c48f1795dc3a7a821fc9e871325cca58f3a
Gamliel Urlanda <g.urlanda@gmail.com>
2023-03-23 18:06:26 -0700

    Add forest assets

---
+++ src/assets/img/backgrounds/forest/ForestLayers.tsx
@@ -0,0 +1,17 @@
+import layer1 from './layer-1.png';
+import layer2 from './layer-2.png';
+import layer3 from './layer-3.png';
+import layer4 from './layer-4.png';
+import layer5 from './layer-5.png';
+import layer6 from './layer-6.png';
+
+const layers = {
+  layer1,
+  layer2,
+  layer3,
+  layer4,
+  layer5,
+  layer6,
+};
+
+export default layers;
