71f4151aa846089a29e6b66176fe228e8cf86ddf
Gamliel Urlanda <g.urlanda@gmail.com>
2023-03-23 18:07:11 -0700

    Refactor parallax layer styles so that they can be generated programmatically

--- src/pages/Sidescroll.tsx
+++ src/pages/Sidescroll.tsx
@@ -1,25 +1,79 @@
-import React, { useState, useEffect, useRef } from 'react';
-import lightBubbles from '../assets/img/backgrounds/pexels-1.jpg';
-import darkBubbles from '../assets/img/backgrounds/pexels-3.jpg';
-import brick from '../assets/img/backgrounds/pexels-2.jpg';
+import React, { useEffect, useRef } from 'react';
+import forestLayers from '../assets/img/backgrounds/forest/ForestLayers';
+
+function parallaxLayerStyle(layerNumber: number): React.CSSProperties {
+  const translateZ = `translateZ(${layerNumber}px)`;
+  const scale = layerNumber !== 0 ? `scale(${layerNumber * -1 + 1})` : '';
+
+  return {
+    position: 'absolute',
+    transform: `${translateZ} ${scale}`,
+    zIndex: layerNumber * 10,
+  };
+}
 
 const Sidescroll: React.FC<{}> = () => {
   const imgContainerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {}, []);
+  function horizontalScrollOnWheelEvent(event: WheelEvent) {
+    if (!imgContainerRef.current) {
+      return;
+    }
+
+    const scrollSpeed = 1.05;
+    imgContainerRef.current.scrollLeft += event.deltaY * scrollSpeed;
+  }
+
+  useEffect(() => {
+    if (!imgContainerRef.current) {
+      return;
+    }
+
+    imgContainerRef.current.addEventListener(
+      'wheel',
+      horizontalScrollOnWheelEvent
+    );
+  }, [imgContainerRef.current]);
 
   return (
-    <div className="parallax " ref={imgContainerRef}>
-      <div className="parallax__layer--base parallax__layer h-[50vh] w-[200vw] bottom-0">
-        <img src={brick} alt="" className="h-full w-full" />
+    <div className="parallax-container " ref={imgContainerRef}>
+      <div
+        className="h-[108px] w-[200vw] bottom-0"
+        style={parallaxLayerStyle(0)}
+      >
+        <img
+          src={forestLayers.layer2}
+          alt=""
+          className=" object-cover h-[108px] bottom-0"
+        />
       </div>
 
-      <div className="parallax__layer parallax__layer--midground  h-[66vh] w-[200vw] -z-10 bottom-0">
-        <img src={lightBubbles} alt="" className=" h-full w-full" />
+      <div
+        className="h-[100vh] w-[200vw] bottom-0"
+        style={parallaxLayerStyle(-1)}
+      >
+        <img src={forestLayers.layer3} alt="" className=" object-cover" />
       </div>
 
-      <div className="parallax__layer parallax__layer--back  h-[100vh] w-[200vw] -z-20 bottom-0">
-        <img src={darkBubbles} alt="" className=" h-full w-full" />
+      <div
+        className="h-[100vh] w-[200vw] bottom-0"
+        style={parallaxLayerStyle(-2)}
+      >
+        <img src={forestLayers.layer4} alt="" className=" object-cover" />
+      </div>
+
+      <div
+        className="h-[100vh] w-[200vw] bottom-0"
+        style={parallaxLayerStyle(-3)}
+      >
+        <img src={forestLayers.layer5} alt="" className=" object-cover" />
+      </div>
+
+      <div
+        className="h-[100vh] w-[200vw] bottom-0"
+        style={parallaxLayerStyle(-4)}
+      >
+        <img src={forestLayers.layer6} alt="" className=" object-cover" />
       </div>
     </div>
   );
