78111c930b5ca30143023d4d147d1993346b8908
Gamliel Urlanda <g.urlanda@gmail.com>
2023-04-06 23:42:27 -0700

    Improve responsiveness of footer

--- src/pages/Home/Footer.tsx
+++ src/pages/Home/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer: React.FC<{}> = () => {
   return (
-    <footer className="relative z-20 w-full h-[10vh] bg-yellow-900 saturate-50 flex justify-center items-center font-roboto font-extralight text-slate-100">
-      <span>
+    <footer className="relative z-20 w-full h-[10vh] min-h-[80px] bg-yellow-900 saturate-50 flex justify-center items-center font-roboto font-extralight text-slate-100">
+      <span className="text-center">
         Website designed and created by{' '}
         <a
           href="https://www.linkedin.com/in/gamliel-urlanda/"
